a777007f980dbcb75740c6a65953d6aa610955d2
Mike Stallard <mikestallard@gmail.com>
2025-11-24 23:20:45 -0500

    Initial Commit

---
+++ AuntieBar/AuntieBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuntieBarApp.swift
+//  AuntieBar
+//
+//  Created by Mike Stallard on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AuntieBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
